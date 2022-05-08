d3b229e650d85ebc2ddd557e6421386a0db472e3
wollbo <hildingw@gmail.com>
2022-05-08 16:04:05 +0200

    provider interface completed, TODO: verify smart contract function with createOption, add database loop to find provider status, add button for creating new provider contract

--- src/components/Provider.js
+++ src/components/Provider.js
@@ -1,59 +1,127 @@
 import React from "react";
-import { Icon, Modal, Card, Button } from "web3uikit";
+import { Icon, Modal, Card, Button, Select, DatePicker, Input, useNotification } from "web3uikit";
 import { useState, useEffect } from "react";
-import {useMoralis} from "react-moralis";
+import { useMoralis, useWeb3ExecuteFunction} from "react-moralis";
 
-function User({account}) {
+function Provider({account}) {
 
   const [isVisible, setVisible] = useState(false);
   const { Moralis } = useMoralis();
-  const [userContracts, setUserContracts] = useState();
+  const [providerContracts, setProviderContracts] = useState();
+  const [priceArea, setPriceArea] = useState("se1");
+  const [contractDate, setContractDate] = useState(new Date());
+  const [payout, setPayout] = useState(1/100);
+  const [fee, setFee] = useState(1/1000);
+  const [strike, setStrike] = useState(100);
+  const contractProcessor = useWeb3ExecuteFunction();
+  const dispatch = useNotification();
 
-  const priceAreaMap = {
-      "se1": "Luleå",
-      "se2": "Sundsvall",
-      "se3": "Stockholm",
-      "se4": "Malmö",
-      "fin": "Finland",
-      "sys": "System"
+  const providerAddress = "0xc42a1d6b87419e66a6670cc3b21b5d020e968e70"
+
+  const handleSuccess= () => {
+    dispatch({
+      type: "success",
+      message: `Successfully evaluated contract`,
+      title: "Evaluation successful",
+      position: "topL"
+    });
   };
 
+  const handleError= (msg) => {
+    dispatch({
+      type: "error",
+      message: `${msg}`,
+      title: "Contract creation failed",
+      position: "topL"
+    });
+  };
 
-  useEffect(() => {
-
-    async function fetchContracts() {
-      const Contracts = Moralis.Object.extend("ContractActive"); // not only listed but ACTIVE contracts
-      const query = new Moralis.Query(Contracts);
-      query.equalTo("clientAddress", account);
-      const result = await query.find();
-
-     
-      setUserContracts(result);
-    }
-
-    fetchContracts();
-  }, [isVisible]);
-
-  function convertDate(epoch) {
-    const d = new Date(epoch);
-    return d.toISOString().slice(0, 10);
+  function convertEpoch(date) {
+    const e = Date.parse(date);
+    return e/1000;
   }
 
-  function convertPriceArea(shortName) {
-    const priceAreaMap = {
-      "se1": "Luleå",
-      "se2": "Sundsvall",
-      "se3": "Stockholm",
-      "se4": "Malmö",
-      "fin": "Finland",
-      "sys": "System"
-    };
-    try {
-      return priceAreaMap.shortName;
+  const createContract = async function(providerAddress, priceArea, startEpoch, duration, fee, payout, strike) { // needs to be linked to and called from a provider contract
+
+
+    let options = {
+      contractAddress: providerAddress,
+      functionName: "createOption",
+      abi: [
+        {
+          "inputs": [
+            {
+              "internalType": "string",
+              "name": "_priceArea",
+              "type": "string"
+            },
+            {
+              "internalType": "uint256",
+              "name": "_startEpoch",
+              "type": "uint256"
+            },
+            {
+              "internalType": "uint256",
+              "name": "_duration",
+              "type": "uint256"
+            },
+            {
+              "internalType": "uint256",
+              "name": "_fee",
+              "type": "uint256"
+            },
+            {
+              "internalType": "uint256",
+              "name": "_payout",
+              "type": "uint256"
+            },
+            {
+              "internalType": "uint256",
+              "name": "_strike",
+              "type": "uint256"
+            }
+          ],
+          "name": "createOption",
+          "outputs": [
+            {
+              "internalType": "address",
+              "name": "",
+              "type": "address"
+            }
+          ],
+          "stateMutability": "payable",
+          "type": "function"
+        }
+      ],
+      params: {
+        providerAddress, 
+        priceArea, 
+        startEpoch, 
+        duration, 
+        fee, 
+        payout,
+        strike
+      },
+      msgValue: payout
     }
-    catch {
-      return shortName;
-    }
+    console.log(providerAddress);
+    console.log(priceArea);
+    console.log(startEpoch);
+    console.log(duration);
+    console.log(fee); 
+    console.log(payout); 
+    console.log(strike); 
+
+
+    await contractProcessor.fetch({
+      params: options,
+      onSuccess: () => {
+        handleSuccess();
+      },
+      onError: (error) => {
+        handleError(error.message)
+      }
+    });
   }
 
   return (
@@ -61,91 +129,132 @@ function User({account}) {
       <div onClick={() => setVisible(true)}>
         <Icon fill="#e49c02" size={28} svg="plus" />
       </div>
-
       <Modal
+        id="provider"
+        onCancel={() => setVisible(false)}
         onCloseButtonPressed={() => setVisible(false)}
         hasFooter={false}
-        title="Your contracts"
+        title="Create new contract"
         isVisible={isVisible}
+        width="330px"
       >
         <div style={{display:"flex", justifyContent:"start", flexWrap:"wrap", gap:"10px"}}>
-          {userContracts &&
-            userContracts.map((e)=>{
-              return(
-                <div style={{ width: "200px"}}>
-                  <Card
-                    isDisabled
-                    title={`${e.attributes.priceArea} ${convertDate(e.attributes.startEpoch*1000)}`} // add convertPriceArea
-                  >
-                    <div>
-                      <img
-                        width="180px"
-                        src="https://ipfs.io/images/ipfs-cluster.png"
-                        />
-                    </div>
-                    <div
-                      style={{
-                        alignItems: 'center',
-                        display: 'flex',
-                        justifyContent:'center',
-                        gap: '5px'
-                      }}
-                    >
-                      <span
-                        style={{
-                          color: '#000000',
-                          fontWeight: 600
-                        }}
-                      >
-                        Strike {e.attributes.strike}  €/MWh
-                      </span>
-                    </div>
-                    <div
-                      style={{
-                        color: '#808080',
-                        display: 'flex',
-                        padding: '2.5px',
-                        gap: '5px',
-                        justifyContent:'flex-end',
-                        fontSize: '12px',
-                        width: '70%'
-                      }}
-                    >
-                      Payout 
-                      <Icon
-                        fill="rgb(228, 156, 2)" 
-                        size={14} 
-                        svg="matic"
-                      />
-                      {e.attributes.payout/10**18}
-                    </div>
-                    <div
-                      style={{
-                        color: '#808080',
-                        display: 'flex',
-                        padding: '2.5px',
-                        gap: '5px',
-                        justifyContent:'flex-end',
-                        fontSize: '12px',
-                        width: '70%'
-                      }}
-                    >
-                      Fee
-                      <Icon
-                        fill="rgb(228, 156, 2)" 
-                        size={14} 
-                        svg="matic"
-                      />{e.attributes.fee/10**18}
-                    </div>
-                    <Button
-                      isFullWidth
-                      text="Evaluate"
-                      theme="primary"
-                    />
-                  </Card> 
+          { // add loop through newProvider in moralis + add button to create new provider (can only be executed if user does not have an active provider in database)
+            <div style={{ width: "200px"}}>
+              <Card
+                isDisabled // add convertPriceArea
+              >
+              <div className="inputs">
+                Price Area
+                <Select
+                  defaultOptionIndex={0}
+                  onChange={(data) => setPriceArea(data.id)}
+                  options={[
+                    {
+                      id: "se1",
+                      label: "Luleå"
+                    },
+                    {
+                      id: "se2",
+                      label: "Sundsvall"
+                    },
+                    {
+                      id: "se3",
+                      label: "Stockholm"
+                    },
+                    {
+                      id: "se4",
+                      label: "Malmö"
+                    },
+                    {
+                      id: "fin",
+                      label: "Finland"
+                    },
+                    {
+                      id: "sys",
+                      label: "System"
+                    },
+                  ]}
+                />
+              </div>
+              <div className="inputs">
+                Contract Date
+                <DatePicker
+                  id="contractDate"
+                  onChange={(event) => setContractDate(event.date)} // really only one date needed, should be hardcoded to setDay
+                  //validation={{min: Date.toISOString().slice(0, 10)}} // add validation later
+                />
+              </div>
+              <div className="inputs">
+                Strike €/MWh
+                <Input
+                  value={100}
+                  name="addStrike"
+                  onChange={(event) => setStrike(event.target.value)}
+                />
+              </div>
+              <div className="inputs">
+                <div
+                  style={{
+                    display: 'flex',
+                    gap: '5px',
+                    justifyContent:'flex-start',
+                    width: '70%'
+                  }}
+                >
+                  Payout 
+                  <Icon
+                    fill="rgb(228, 156, 2)" 
+                    size={14} 
+                    svg="matic"
+                  />
                 </div>
-              )
-            })
+                <Input
+                  value={1/100}
+                  name="addPayout"
+                  type="number"
+                  onChange={(event) => setPayout(Number(event.target.value*10**18))}
+                />
+              </div>
+              <div className="inputs">
+                <div
+                  style={{
+                    display: 'flex',
+                    gap: '5px',
+                    justifyContent:'flex-start',
+                    width: '70%'
+                  }}
+                >
+                  Fee
+                  <Icon
+                    fill="rgb(228, 156, 2)" 
+                    size={14} 
+                    svg="matic"
+                  />
+                </div>
+                <Input
+                  value={1/1000}
+                  name="addFee"
+                  type="number"
+                  onChange={(event) => setFee(Number(event.target.value*10**18))}
+                />
+              </div>
+              <Button
+                onClick={() => createContract(
+                  providerAddress,
+                  priceArea,
+                  convertEpoch(contractDate),
+                  86400,
+                  fee,
+                  payout,
+                  strike
+                )}
+                isFullWidth
+                text="Create"
+                theme="primary"
+              />
+            </Card> 
+          </div>
           }
         </div>
       </Modal>
@@ -153,4 +262,4 @@ function User({account}) {
   );
 }
 
-export default User;
+export default Provider;
