ec137c255c9e07e54ce7e1867653ee32ee4b561f
wollbo <34742686+wollbo@users.noreply.github.com>
2022-05-27 09:56:30 +0200

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -42,8 +42,8 @@ const Home = () => {
       </div> 
       <div className="tabContent">
         <div className="searchFields">
-          <body class=inputBody">
-            <div class="inputs">
+          <body className=inputBody">
+            <div className="inputs">
               Price Area
               <Select
                 defaultOptionIndex={0}
@@ -78,8 +78,8 @@ const Home = () => {
             </div>
           </div>
           <div className="vl"/>
-          <body class=inputBody">
-            <div class="inputs">
+          <body className=inputBody">
+            <div className="inputs">
               Contract Date
               <DatePicker
                 id="contractDate"
@@ -89,8 +89,8 @@ const Home = () => {
             </div>
           </div>
           <div className="vl" />
-          <body class=inputBody">
-            <div class="inputs">
+          <body className=inputBody">
+            <div className="inputs">
               Energy (MWh)
               <Input
                 value={1}
